16e8b402a94ad6151c3f3786fb56b3b2bf3ba496
RAON <jungmini0601@gmail.com>
2023-01-19 10:18:30 +0900

    gatsby 튜토리얼 종료

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+import Seo from "../components/Seo";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   );
 };
 
-export const Head = () => <title>Home Page1</title>;
+export const Head = () => <Seo title="Home Page" />;
 
 export default IndexPage;
